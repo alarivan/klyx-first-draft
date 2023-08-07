0b853d78f9359489bd7860f09eac0bcefcf347af
A Larivan <alarivan@pm.me>
2023-08-07 02:19:15 -0700

    Show lists on home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import { A } from "@solidjs/router";
 import { FiPlus } from "solid-icons/fi";
-import { Component, Show } from "solid-js";
+import { Component, Show, For } from "solid-js";
 
 import { useStoreContext } from "../../store/context";
 
@@ -10,17 +10,23 @@ export const Home: Component = () => {
   const [state] = useStoreContext();
 
   return (
-      <Show
-        when={state.lists.length > 0}
-        fallback={
-          <div class={styles.addListButtonWrapper}>
-            <A href='/list/new' class={styles.addListButton}>
-              add new list <FiPlus style={{ display: "block" }} size={40} />
-            </A>
-          </div>
-        }
-      >
-        list with items
-      </Show>
+    <Show
+      when={state.lists.length > 0}
+      fallback={
+        <div class={styles.addListButtonWrapper}>
+          <A href='/list/new' class={styles.addListButton}>
+            add new list <FiPlus style={{ display: "block" }} size={40} />
+          </A>
+        </div>
+      }
+    >
+      <ul>
+        <For each={state.lists}>
+          {(list) => (
+            <li>{list.name}</li>
+          )}
+        </For>
+      </ul>
+    </Show>
   );
 };
