f4916dd116f0daea9b0d2b3b3371c81b50847c23
A Larivan <alarivan@pm.me>
2023-08-09 03:04:53 -0700

    Add initial store

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { render } from "solid-js/web";
 import "./index.css";
 import App from "./App";
 import { StoreProvider } from "./store/context";
+import { createListWithItems } from "./store/helpers";
 
 const root = document.getElementById("root");
 
@@ -14,10 +15,24 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   );
 }
 
+const lists = Array(4)
+  .fill(undefined)
+  .map((_, lidx) =>
+    createListWithItems(
+      { name: `list${lidx + 1}`, description: `list${lidx}dec` },
+      Array(4)
+        .fill(undefined)
+        .map((_, idx) => ({
+          name: `item${idx + 1}name`,
+          description: `item${idx + 1}desc`,
+        })),
+    ),
+  );
+
 render(
   () => (
     <Router>
-      <StoreProvider>
+      <StoreProvider initalStore={{ lists }}>
         <App />
       </StoreProvider>
     </Router>
