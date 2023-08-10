b9ae3d0d25783102f947ce1e8313d37926564b4c
A Larivan <alarivan@pm.me>
2023-08-09 23:55:08 -0700

    Add links & guard to PlayDone

--- src/pages/PlayDone/PlayDone.test.tsx
+++ src/pages/PlayDone/PlayDone.test.tsx
@@ -1,13 +1,74 @@
+import type { Mock } from "vitest";
+
+import { Router, useNavigate, useParams } from "@solidjs/router";
 import { render, screen } from "@solidjs/testing-library";
 import { describe, expect, it } from "vitest";
 import "@testing-library/jest-dom";
 
+import { StoreProvider } from "../../store/context";
+import { createListWithItems } from "../../store/helpers";
+
 import { PlayDone } from "./PlayDone";
 
-describe("PlayDone", () => {
-  it("", () => {
-    render(() => <PlayDone />);
+const list = createListWithItems({ name: "list1", description: "list1desc" }, [
+  { name: "item1", description: "item1desc" },
+]);
 
-    expect(screen.getByText("done")).toBeInTheDocument();
+vi.mock("@solidjs/router", async () => {
+  const mod: any = await vi.importActual("@solidjs/router");
+  return {
+    ...mod,
+    useParams: vi.fn(),
+    useNavigate: vi.fn(),
+  };
+});
+
+const mockUseParams = useParams as Mock;
+const mockUseNavigate = useNavigate as Mock;
+
+describe("PlayDone", () => {
+  beforeEach(() => {
+    mockUseParams.mockReturnValue({ listId: list.id });
+    mockUseNavigate.mockReturnValue(vi.fn());
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("renders component", () => {
+    render(() => (
+      <Router>
+        <StoreProvider initalStore={{ lists: [list] }}>
+          <PlayDone />
+        </StoreProvider>
+      </Router>
+    ));
+
+    expect(screen.getByText(list.name)).toBeInTheDocument();
+    expect(screen.getByText("Restart")).toHaveAttribute(
+      "href",
+      `/list/${list.id}/play`,
+    );
+    expect(screen.getByText("Go to list")).toHaveAttribute(
+      "href",
+      `/list/${list.id}`,
+    );
+  });
+
+  it("redirects when list is not found", () => {
+    mockUseParams.mockReturnValue({ listId: "fake" });
+    const mockNavigate = vi.fn();
+    mockUseNavigate.mockReturnValue(mockNavigate);
+
+    render(() => (
+      <Router>
+        <StoreProvider initalStore={{ lists: [list] }}>
+          <PlayDone />
+        </StoreProvider>
+      </Router>
+    ));
+
+    expect(mockNavigate).toHaveBeenCalledOnce();
   });
 });
--- src/pages/PlayDone/PlayDone.tsx
+++ src/pages/PlayDone/PlayDone.tsx
@@ -1,5 +1,33 @@
 import type { Component } from "solid-js";
 
+import { A, useNavigate, useParams } from "@solidjs/router";
+import { createEffect } from "solid-js";
+
+import { useStoreContext } from "../../store/context";
+
 export const PlayDone: Component = () => {
-  return <div>done</div>;
+  const params = useParams();
+  const navigate = useNavigate();
+  const [_, actions] = useStoreContext();
+
+  const maybeList = () => actions.find(params.listId);
+
+  createEffect(() => {
+    const list = maybeList();
+    if (!list) {
+      navigate("/", { replace: true });
+    }
+  });
+
+  return (
+    <div>
+      <h1>Done</h1>
+      <h3>{maybeList()?.name}</h3>
+      <A href="/">Go home</A>
+      <br />
+      <A href={`/list/${params.listId}`}>Go to list</A>
+      <br />
+      <A href={`/list/${params.listId}/play`}>Restart</A>
+    </div>
+  );
 };
