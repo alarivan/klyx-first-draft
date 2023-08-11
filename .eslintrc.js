c1ae70e76cc5991cd58d9c55c13b495e1ddefcb3
A Larivan <alarivan@pm.me>
2023-08-10 23:02:23 -0700

    Fix indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,71 +1,71 @@
 module.exports = {
-    root: true,
-    parser: "@typescript-eslint/parser",
-    plugins: ["solid", "prettier", "@typescript-eslint"],
-    extends: [
-        "eslint:recommended",
-        "plugin:solid/typescript",
-        "prettier",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-    ],
-    env: {
-        browser: true,
-        es2021: true,
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["solid", "prettier", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:solid/typescript",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{ts,tsx,js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: [".eslintrc.{ts,tsx,js,cjs}"],
-            parserOptions: {
-                sourceType: "script",
-            },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "type",
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order:
+            "asc" /* sort in ascending order. Options: ['ignore', 'asc', 'desc'] */,
+          caseInsensitive: true /* ignore case. Options: [true, false] */,
         },
+      },
     ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    rules: {
-        "prettier/prettier": "error",
-        "arrow-body-style": "off",
-        "prefer-arrow-callback": "off",
-        "import/order": [
-            "error",
-            {
-                groups: [
-                    "type",
-                    "builtin",
-                    "external",
-                    "internal",
-                    "parent",
-                    "sibling",
-                    "index",
-                ],
-                "newlines-between": "always",
-                alphabetize: {
-                    order:
-                        "asc" /* sort in ascending order. Options: ['ignore', 'asc', 'desc'] */,
-                    caseInsensitive: true /* ignore case. Options: [true, false] */,
-                },
-            },
-        ],
-        "@typescript-eslint/no-unused-vars": [
-            "warn",
-            {
-                argsIgnorePattern: "^_",
-                varsIgnorePattern: "^_",
-                caughtErrorsIgnorePattern: "^_",
-            },
-        ],
-        "@typescript-eslint/consistent-type-imports": [
-            "error",
-            {
-                prefer: "type-imports",
-            },
-        ],
-    },
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        prefer: "type-imports",
+      },
+    ],
+  },
 };
