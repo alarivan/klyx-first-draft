5022e299b9407ca2d8825f5da179a43f36b1b61f
A Larivan <alarivan@pm.me>
2023-08-25 09:04:12 -0700

    Add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solidPlugin({ typescript: { onlyRemoveTypeImports: true } })],
+  plugins: [
+    solidPlugin({ typescript: { onlyRemoveTypeImports: true } }),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
   server: {
     port: 3000,
   },
