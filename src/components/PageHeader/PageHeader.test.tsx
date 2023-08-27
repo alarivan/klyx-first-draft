930ce6b5c7242653811490adb040aa144e6490b2
A Larivan <alarivan@pm.me>
2023-08-27 02:28:49 -0700

    Add wake lock to page header

--- src/components/PageHeader/PageHeader.test.tsx
+++ src/components/PageHeader/PageHeader.test.tsx
@@ -1,15 +1,80 @@
-import { screen } from "@solidjs/testing-library";
+import type { Mock } from "vitest";
+
+import { fireEvent, screen } from "@solidjs/testing-library";
 import { describe, expect, it } from "vitest";
 
+import { createWakeLock } from "../../lib/wakeLock";
 import { renderInRouter } from "../../test/utils";
 
 import { PageHeader } from "./PageHeader";
 
+vi.mock("../../lib/wakeLock", async () => {
+  const type = await import("../../lib/wakeLock");
+  const mod: typeof type = await vi.importActual("../../lib/wakeLock");
+  return {
+    ...mod,
+    createWakeLock: vi.fn(),
+  };
+});
+
+const createWakeLockMock = createWakeLock as Mock;
+
 describe("PageHeader", () => {
+  beforeEach(() => {
+    createWakeLockMock.mockReturnValue([() => false, false, vi.fn(), vi.fn()]);
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   it("renders component", () => {
     renderInRouter(() => <PageHeader />);
 
     expect(screen.getByTitle("Home")).toBeInTheDocument();
     expect(screen.getByTitle("Settings")).toBeInTheDocument();
+  });
+
+  describe("when wake lock is available ", () => {
+    const requestMock = vi.fn();
+    const releaseMock = vi.fn();
+    beforeEach(() => {
+      createWakeLockMock.mockReturnValue([
+        () => true,
+        true,
+        requestMock,
+        releaseMock,
+      ]);
+    });
+
+    it("renders wake checkbox when wake lock is available", () => {
+      renderInRouter(() => <PageHeader />);
+
+      expect(screen.getByLabelText("Keep screen awake")).toBeChecked();
+    });
+
+    it("calls request mock when not active", () => {
+      createWakeLockMock.mockReturnValue([
+        () => false,
+        true,
+        requestMock,
+        releaseMock,
+      ]);
+      renderInRouter(() => <PageHeader />);
+
+      const toggle = screen.getByLabelText("Keep screen awake");
+      fireEvent.click(toggle);
+
+      expect(requestMock).toHaveBeenCalled();
+    });
+
+    it("calls release mock when active", () => {
+      renderInRouter(() => <PageHeader />);
+
+      const toggle = screen.getByLabelText("Keep screen awake");
+      fireEvent.click(toggle);
+
+      expect(releaseMock).toHaveBeenCalled();
+    });
   });
 });
--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -2,10 +2,24 @@ import type { Component } from "solid-js";
 
 import { A } from "@solidjs/router";
 import { FiSettings } from "solid-icons/fi";
+import { Show } from "solid-js";
+
+import { createWakeLock } from "../../lib/wakeLock";
 
 import styles from "./PageHeader.module.css";
 
 export const PageHeader: Component = () => {
+  const [isActive, isAvailable, requestWakeLock, releaseWakeLock] =
+    createWakeLock();
+
+  const toggleWakeLock = () => {
+    if (isActive()) {
+      releaseWakeLock();
+    } else {
+      requestWakeLock();
+    }
+  };
+
   return (
     <div class={styles.container}>
       <A title="Home" class={`a-reset`} href="/">
@@ -16,13 +30,26 @@ export const PageHeader: Component = () => {
           height={30}
         />
       </A>
-      <A
-        title="Settings"
-        class={`a-reset action action__secondary ${styles.settings}`}
-        href="/settings"
-      >
-        <FiSettings size={22} />
-      </A>
+      <div class={styles.actions}>
+        <Show when={isAvailable}>
+          <div class={`inputGroup__checkbox ${styles.wakeLockInput}`}>
+            <input
+              checked={isActive()}
+              onChange={toggleWakeLock}
+              id="wakeLock"
+              type="checkbox"
+            />
+            <label for="wakeLock">Keep screen awake</label>
+          </div>
+        </Show>
+        <A
+          title="Settings"
+          class={`a-reset action action__secondary ${styles.action}`}
+          href="/settings"
+        >
+          <FiSettings size={22} />
+        </A>
+      </div>
     </div>
   );
 };
