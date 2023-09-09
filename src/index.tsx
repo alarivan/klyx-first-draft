4fcaaca7f072f57aeade79206e9fa5a3b8d26789
A Larivan <alarivan@pm.me>
2023-09-09 03:11:06 -0700

    Seed initial state when not in local storage

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { render } from "solid-js/web";
 import "modern-normalize";
 import "./index.css";
 import App from "./App";
+import { restoreToInitalState } from "./lib/backup";
+import { LOCAL_STORAGE_STORE_KEY } from "./store";
 import { StoreProvider } from "./store/context";
 
 const root = document.getElementById("root");
@@ -13,6 +15,10 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
     "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?",
   );
+}
+
+if (!localStorage.getItem(LOCAL_STORAGE_STORE_KEY)) {
+  restoreToInitalState();
 }
 
 render(
